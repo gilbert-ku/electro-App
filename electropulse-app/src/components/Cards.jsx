4a409206a218d266922aa37d31f77cddd2a30d27
gilbert-ku <gilbert45ku@gmail.com>
2023-10-28 01:38:27 +0300

    fetch data and map through categories

--- electropulse-app/src/components/Cards.jsx
+++ electropulse-app/src/components/Cards.jsx
@@ -1,14 +1,55 @@
 
-import React from "react";
-imoprt 
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Cards = () => {
-    return (
-        <div className="container">
-            
-        </div>
-    )
-}
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-export default Cards
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://kohls.p.rapidapi.com/categories/list',
+        headers: {
+          'X-RapidAPI-Key': 'c355c9c215msh4ab47fb85d1669fp139939jsnbb46094711c3',
+          'X-RapidAPI-Host': 'kohls.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        console.log(setData(response.data));
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+return (
+    <div className="container">
+        {/* handles error message */}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <div className="bg-light">
+          {data.payload.categories.map((category) => (
+            <div key={category.ID} className="container text-center my-5">
+                {/* category name */}
+                <div className="container bg-warning rounded-top-3 mb-3">
+                    <h1>{category.name}</h1>
+                </div>
+              
+                
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+
+}
+export default Cards;
+
+

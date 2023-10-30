66b7817e9d2aa627f4038b36bfc46c10d1613665
Daniel <owenjodaniel19@gmail.com>
2023-10-30 12:21:42 +0300

    fixed the logo styling

--- electropulse-app/src/components/Navbar.jsx
+++ electropulse-app/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand">
-          <span className="shop-icon">Electropulse</span>
+          <p className="shop-icon">electro<span>Pulse</span></p>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>

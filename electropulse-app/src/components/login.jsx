d6c7e7e07b7f2fedf5c49a9a9dffbab3a7a24793
gilbert-ku <gilbert45ku@gmail.com>
2023-10-30 11:43:29 +0300

    add to new branch

---
+++ electropulse-app/src/components/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <button type="button" class="btn btn-outline-primary">Primary</button>
+           
+            
+        </div>
+    )
+}
+export default Login

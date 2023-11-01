8432606fdbd89c624325c7c434dc21908c04bfb9
misra2023 <mishazabdi@gmail.com>
2023-11-01 21:52:03 +0300

    completed about us contact us and helpcentre

--- electropulse-app/src/App.jsx
+++ electropulse-app/src/App.jsx
@@ -1,25 +1,29 @@
-// import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import Home from '../src/components/Home'
-import Login from '../src/components/login';
-import Signup from '../src/components/signup';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../src/components/Home';
+import Login from '../src/components/Login';
+import Signup from '../src/components/Signup';
+import AboutUs from '../src/components/AboutUs';
+import ContactUs from '../src/components/ContactUs';
+import HelpCentre from '../src/components/HelpCentre';
+//import Navigation from '../src/components/Navigation';
 
-
-
-import './App.css'
+import './App.css';
 
 const App = () => {
-
   return (
     <div>
+      {/* /*<Navigation /> */} 
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/help-centre" element={<HelpCentre />} />
       </Routes>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;

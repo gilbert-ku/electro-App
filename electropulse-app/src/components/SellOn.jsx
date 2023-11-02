8abb22437fee9177a6014101388fc34d0d45937a
Daniel <owenjodaniel19@gmail.com>
2023-11-02 22:06:36 +0300

    sell on route

--- electropulse-app/src/components/SellOn.jsx
+++ electropulse-app/src/components/SellOn.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import  Image from "../assets/image/sell.jpg"
+import React, { useState } from "react";
+import Image from "../assets/image/sell.jpg";
+
 
 const SellOn = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <section className="sell-on-section">
             <div className="logo-container">
@@ -9,9 +16,30 @@ const SellOn = () => {
             </div>
             <div className="text-container">
                 <h2 className="sell-text">Do you want to sell and make money on ElectroPulse?</h2>
-                <button className="sell-button" ><span>Start</span></button>
+                <button className="sell-button" onClick={toggleModal}>
+                    <span>Start</span>
+                </button>
             </div>
+            {showModal && (
+                <div className="modal-container">
+                    <div className="modal">
+                        <span className="close" onClick={toggleModal}>&times;</span>
+                        <h2>Fill in your information</h2>
+                        <form>
+                            <input type="text" placeholder="Name"/>
+                            <input type="email" placeholder="Email"/>
+                            <input type="text" placeholder="Contact"/>
+                            <input type="password" placeholder="Password"/>
+                        </form>
+                        <div className="modal-buttons">
+                            <button className="modal-close" onClick={toggleModal}>Close</button>
+                            <button className="modal-proceed">Proceed</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
-    )
-}
+    );
+};
+
 export default SellOn;

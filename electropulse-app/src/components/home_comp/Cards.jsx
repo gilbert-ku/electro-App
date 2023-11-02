ec52a26016b3630dd53fe9b372a6ab7dd79ef15f
gilbert-ku <gilbert45ku@gmail.com>
2023-11-02 17:14:22 +0300

    render selected cards

--- electropulse-app/src/components/home_comp/Cards.jsx
+++ electropulse-app/src/components/home_comp/Cards.jsx
@@ -45,13 +45,33 @@ const Cards = () => {
       {/* Recently Viewed*/}
       <div className='container w-100 '>
         <p>Recently Viewed</p>
-          
+          <div className='container'>
+              {selectedCards.map((selectedCardId) => {
+                const selectedCard = data.find(card => card.id === selectedCardId);
+                return (
+                  <div className="col" key={selectedCardId}>
+                    {/* cards */}
+                    <div 
+                      className="card border border-warning shadow p-3 mb-5 bg-body rounded"
+                      style={{ width: '15rem', height: "22rem"}}
+                    >
+                      <img src={selectedCard.image_url} className="card-img-top" alt="..." />
+                      <div className="card-body">
+                        <p className="card-text">{selectedCard.brand}</p>
+                        <p className="card-title">{selectedCard.name}</p>
+                        <p className="card-text">Ksh: {selectedCard.price}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
         </div>
 
     
         {/* products rendering  */}
 
-
+        
       <div className='container'>
         <div className='container  w-100 '>
           {/* <h1 className='bg-warning rounded-top p-2'>Recently Viewed</h1> */}
@@ -88,63 +108,3 @@ const Cards = () => {
 }  
 export default Cards;
 
-
-// import React, { useEffect, useState } from 'react';
-// import Slider from 'react-slick';
-// import axios from 'axios';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
-
-// const Cards = () => {
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get("http://127.0.0.1:5555/products");
-//         setData(response.data);
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//         setError("Error fetching data");
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   // Add autoplay functionality using the state
-//   const [autoplay, setAutoplay] = useState(true);
-
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplay: autoplay,
-//     autoplaySpeed: 2000,
-//   };
-
-//   return (
-//     <div>
-//       <h2>Auto Play & Pause without buttons</h2>
-//       <Slider {...settings}>
-//         {data &&
-//           data.map((d) => (
-//             <div className="card" style={{ width: '12rem' }} key={d.id}>
-//               <img src={d.image_url} className="card-img-top h-50" alt="..." />
-//               <div className="card-body">
-//                 <p className="card-text">{d.brand}</p>
-//                 <h5 className="card-title">{d.name}</h5>
-//                 {/* <p className="card-text">{d.description}</p> */}
-//                 <p className="card-text">Ksh: {d.price}</p>
-//               </div>
-//             </div>
-//           ))}
-//       </Slider>
-//     </div>
-//   );
-// };
-
-// export default Cards;
